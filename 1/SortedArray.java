c4428933c8c42946bf675984f0e0fe0ddaafab65
FromSi <fromsi665@gmail.com>
2018-09-07 01:02:12 +0600

    Added comments

--- 1/SortedArray.java
+++ 1/SortedArray.java
@@ -3,29 +3,34 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class SortedArray {
+    //Для ввода
     private static Scanner scanner = new Scanner(System.in);
 
+    //Главный метод
     public static void main(String[] args) {
-
+        //Покажет результат
         System.out.println("\n Sorted: " + Arrays.toString(handler(initArray(scanner.nextInt()))));
     }
 
+    //Метод инициализации
     private static int[] initArray(int size){
         int[] mas = new int[size];
         Random random = new Random(System.currentTimeMillis());
 
         for (int i = 0; i < size; i++)
-            mas[i] = random.nextInt(256);
+            mas[i] = random.nextInt(256); // от 0 до 255
 
-        System.out.println("\n Random: " + Arrays.toString(mas));
+        //Показывает результат инициализации массива
+        System.out.println("\n Random: " + Arrays.toString(mas)); 
 
         return mas;
     }
 
+    //Обработчик. Выполняет сортировку пузырьковым методом
     private static int[] handler(int[] mas) {
         for (int i = 0; i < mas.length - 1; i++)
             for (int j = 0; j < mas.length - 1; j++)
-                if (mas[j] > mas[j + 1]) {
+                if (mas[j] > mas[j + 1]) { // Если 1-й элемент больше 2-го, то меняем местами.
                     int a = mas[j];
                     mas[j] = mas[j + 1];
                     mas[j + 1] = a;
--- 2/ScatterArray.java
+++ 2/ScatterArray.java
@@ -3,24 +3,33 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ScatterArray {
+    //Для ввода
     private static Scanner scanner = new Scanner(System.in);
     private static Random random = new Random(System.currentTimeMillis());
-
+    
+    //Главный метод
     public static void main(String[] args) {
+        //Покажет результат
         System.out.println("\n Sorted: " + Arrays.toString(handler(initArray(scanner.nextInt()))));
     }
 
+    //Метод инициализации
     private static int[] initArray(int size) {
         int[] mas = new int[size];
 
         for (int i = 0; i < size; i++)
-            mas[i] = i + 1;
+            mas[i] = i + 1; // от 1 до n
 
+        //Показывает результат инициализации массива
         System.out.println("\n Random: " + Arrays.toString(mas));
 
         return mas;
     }
 
+   /** 
+    *  Обработчик. Каждый шаг, мы записываем рандомный элемент со старого массива в новый
+    *  и удаляем элемент выбранного массива у старого элемента
+    */
     private static int[] handler(int[] mas) {
         int[] newMas = new int[mas.length];
 
@@ -33,6 +42,7 @@ public class ScatterArray {
         return newMas;
     }
 
+    //Удаляет элемент и возвращает новый массив
     private static void removeElement(int[] arr, int removedIdx) {
         System.arraycopy(arr, removedIdx + 1, arr, removedIdx, arr.length - 1 - removedIdx);
     }
